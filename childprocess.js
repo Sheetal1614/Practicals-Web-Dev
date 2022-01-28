5caa28f3fa997264a0ff8c2eb3e46227f18ad62f
Sheetal <56538155+Sheetal1614@users.noreply.github.com>
2022-01-28 13:23:46 +0530

    Delete childprocess.js

--- childprocess.js
+++
@@ -1,7 +0,0 @@
-let cp = require("child_process");
-console.log("Trying to open google 🧡");
-//cp.execSync("calc");
-cp.execSync("start chrome https://google.com");
-console.log("opened google");
-let output = cp.execSync("node example.js");
-console.log("output"+ output);
