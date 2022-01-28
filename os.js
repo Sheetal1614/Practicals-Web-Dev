52e14ab8854a2bcc9e6014717d618f720d80ab39
Sheetal <56538155+Sheetal1614@users.noreply.github.com>
2022-01-28 13:24:37 +0530

    Delete os.js

--- os.js
+++
@@ -1,9 +0,0 @@
-//os ke feature dene  liye yeh module
-let os = require("os");
-//console.log(os.arch());
-//console.log(os.platform());
-
-//wifi i information  liye
-//console.log(os.networkInterfaces());
-//to know server load
-console.log(os.cpus()); 
